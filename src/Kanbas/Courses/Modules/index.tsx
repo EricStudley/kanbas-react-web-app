b4af2b1b786442abd2bc14aedbe530a489577164
Eric Studley <eric.studley@gmail.com>
2024-05-16 14:34:54 -0400

    Work for assignment 1

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -1,29 +1,82 @@
 export default function Modules() {
     return (
-      <div>
-        {/* Collapse All button, View Progress button, etc. */}
-        <ul id="wd-modules">
-          <li className="wd-module">
-            <div className="wd-title">Week 1</div>
-            <ul className="wd-lessons">
-              <li className="wd-lesson">
-                <span className="wd-title">LEARNING OBJECTIVES</span>
-                <ul className="wd-content">
-                  <li className="wd-content-item">Introduction to the course</li>
-                  <li className="wd-content-item">Learn what is Web Development</li>
-                </ul>
-              </li>
+        <div>
+            <button id="wd-collapse-all">Collapse All</button>{" "}
+            <button id="wd-view-progress">View Progress</button>{" "}
+            <select id="wd-filter">
+                <option selected value="all">
+                    Publish All
+                </option>
+                <option value="completed">Publish Completed</option>
+            </select>{" "}
+            <button id="wd-add-module">+ Module</button>
+            <ul id="wd-modules">
+                <li>
+                    <div>
+                        Week 1, Lecture 1 - Course Introduction, Syllabus,
+                        Agenda
+                    </div>
+                    <ul>
+                        <li>
+                            <span>LEARNING OBJECTIVES</span>
+                            <ul>
+                                <li>Introduction to the course</li>
+                                <li>Learn what is Web Development</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <span>READING</span>
+                            <ul>
+                                <li>
+                                    Full Stack Developer - Chapter 1 -
+                                    Introduction
+                                </li>
+                                <li>
+                                    Full Stack Developer - Chapter 2 - Creating
+                                    User
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <span>SLIDES</span>
+                            <ul>
+                                <li>Introduction to Web Development</li>
+                                <li>Creating an HTTP server with Node.js</li>
+                                <li>Creating a React Application</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <div>
+                        Week 1, Lecture 2 - Formatting User Interfaces with HTML
+                    </div>
+                    <ul>
+                        <li>
+                            <span>LEARNING OBJECTIVES</span>
+                            <ul>
+                                <li>
+                                    Learn how to create user interfaces with
+                                    HTML
+                                </li>
+                                <li>Deploy the assignment to Netlify</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <span>SLIDES</span>
+                            <ul>
+                                <li>Introduction to HTML and the DOM</li>
+                                <li>
+                                    Formatting Web content with Headings and
+                                </li>
+                                <li>
+                                    Formatting content with Lists and Tables
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
             </ul>
-          </li>
-          <li className="wd-module">
-            <div className="wd-title">Week 2</div>
-            <ul className="wd-lessons">
-              <li className="wd-lesson">
-                <span className="wd-title">LEARNING OBJECTIVES</span>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-  );}
-  
+        </div>
+    );
+}
